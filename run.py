81a8f3a0d85d83f8f0fbdd3f344d3c345a2b7a20
michelleblom <michelle.blom@gmail.com>
2019-10-08 15:35:33 +1100

    Added license comments to each file

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
+# Written by Michelle Blom, 2019
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 from model import GameRunner,Player
 from iplayer import InteractivePlayer
 from naive_player import NaivePlayer
 
 
-players = [InteractivePlayer(0), NaivePlayer(1)]
+players = [InteractivePlayer(0), NaivePlayer(1), NaivePlayer(2),NaivePlayer(3)]
 
 gr = GameRunner(players, 1384754856864)
 
@@ -11,3 +26,5 @@ scores = gr.Run(True)
 
 print("Player 0 score is {}".format(scores[0]))
 print("Player 1 score is {}".format(scores[1]))
+print("Player 2 score is {}".format(scores[2]))
+print("Player 3 score is {}".format(scores[3]))
--- utils.py
+++ utils.py
@@ -1,5 +1,19 @@
+# Written by Michelle Blom, 2019
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 from enum import IntEnum
-
 
 # There are 5 types of tiles in the game, each differentiated by colour.
 class Tile(IntEnum):
